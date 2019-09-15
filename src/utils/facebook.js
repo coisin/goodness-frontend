f12395db0b10b6be1c0c5abfe9990cd4abd2fd63
theLAZYmd <lazymeep+github@gmail.com>
2019-09-15 06:40:16 -0400

    Wrong location for fb.init

--- src/utils/facebook.js
+++ src/utils/facebook.js
@@ -1,8 +1,8 @@
 module.exports = () => {
-	FB.login(function(response) {
+	window.FB.login(function(response) {
 		if (response.authResponse) {
 		console.log('Welcome!  Fetching your information.... ');
-		FB.api('/me', function(response) {
+		window.FB.api('/me', function(response) {
 		console.log('Good to see you, ' + response.name + '.');
 		});
 		} else {
