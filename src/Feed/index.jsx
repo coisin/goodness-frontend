13f92ab73dc81fb0d171c776269fcf1ad17984fb
Oisín O'Dwyer <odwyeroisin@gmail.com>
2019-09-15 11:29:12 +0100

    Buttons and whatnote

--- src/Feed/index.jsx
+++ src/Feed/index.jsx
@@ -1,14 +1,68 @@
 import React from 'react';
 
+import {LineChart, XAxis, YAxis, Line, CartesianGrid} from 'recharts';
+import Auth from '../utils/auth.js';
+
 export default class Feed extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      width: null
+    };
+    this.data = [
+      {
+        day: 'Monday', score: Math.random() * 1000
+      },
+      {
+        day: 'Tuesday', score: Math.random() * 1000
+      },
+      {
+        day: 'Wednesday', score: Math.random() * 1000
+      },
+      {
+        day: 'Thursday', score: Math.random() * 1000
+      },
+      {
+        day: 'Friday', score: Math.random() * 1000
+      },
+      {
+        day: 'Saturday', score: Math.random() * 1000
+      },
+      {
+        day: 'Sunday', score: Math.random() * 1000
+      }
+    ];
+    this.connectBank = this.connectBank.bind(this);
   }
   render() {
     return <div className='block'>
-      <div className='feed'>
-        
+      <div className='feed' ref={el => {this.container = el}}>
+      <div className='lineChartContainer'>
+      <LineChart width={this.state.width * .9} height={300} data={this.data}>
+        <XAxis dataKey="day"/>
+        <YAxis/>
+        <CartesianGrid stroke="#eee" strokeDasharray="5 5"/>
+        <Line type="monotone" dataKey="score" stroke="#7cadfc" />
+      </LineChart>
       </div>
+      <button onClick={this.connectBank}>Connect Your Bank</button>
     </div>
+  </div>
+  }
+  setContainerWidth() {
+    var style = window.getComputedStyle(this.container, null);
+    let width = style.getPropertyValue("width");
+    width = parseInt(width.substr(0, width.length - 2));
+    this.setState({width});
+  }
+  componentDidMount() {
+    this.setContainerWidth();
+    window.addEventListener('resize', () => {
+      this.setContainerWidth();
+    });
+  }
+
+  connectBank() {
+    Auth('Hello World');
   }
 }
